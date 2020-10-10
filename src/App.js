c6e74514437f3270c0881384df301f31d0b90415
b_bado <bado.bernard94@gmail.com>
2020-10-10 20:32:06 +0200

    finishing changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useSpring, animated } from "react-spring";
 
 const App = () => {
@@ -7,10 +7,9 @@ const App = () => {
   const interpolateCount = xy.interpolate(
     (x, y) =>
       `perspective(200px) translate(${x / 100}px, ${y / 100}px) rotateY(${
-        x / 50
-      }deg) rotateX(${-y / 50}deg)`
+        x / 80
+      }deg) rotateX(${-y / 80}deg)`
   );
-  const buttonRef = useRef(null);
   const onMove = useCallback(
     ({ clientX: x, clientY: y }) =>
       set({ xy: [x - window.innerWidth / 2, y - window.innerHeight / 2] }),
@@ -18,36 +17,41 @@ const App = () => {
   );
   const onScroll = useCallback((e) => set({ st: e.target.scrollTop / 30 }), []);
 
-  const decreaseCount = () => count && setCount(count - 1);
-  const increaseCount = ({ target }) =>
-    target !== buttonRef.current && setCount((count) => count + 1);
+  const handleClick = ({ clientX }) => {
+    if (clientX - window.innerWidth / 2 > 0) {
+      setCount(count + 1);
+    } else {
+      count && setCount(count - 1);
+    }
+  };
+
+  const handleKeyDown = () => {
+    setCount((count) => count + 1);
+  };
 
   // Listen for any click or key
   useEffect(() => {
-    window.addEventListener("click", increaseCount);
-    window.addEventListener("keydown", increaseCount);
+    window.addEventListener("keydown", handleKeyDown);
 
     return () => {
-      window.removeEventListener("click", increaseCount);
-      window.removeEventListener("keydown", increaseCount);
+      window.removeEventListener("keydown", handleKeyDown);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div id="container" onMouseMove={onMove} onScroll={onScroll}>
+    <div
+      id="container"
+      onMouseMove={onMove}
+      onScroll={onScroll}
+      onClick={handleClick}
+    >
+      <span id="plus">+</span>
+      <span id="minus">-</span>
       <div id="counter">
         <animated.h2 style={{ transform: interpolateCount }}>
           {count}
         </animated.h2>
-        <span
-          ref={buttonRef}
-          role="img"
-          aria-label="Decrease count"
-          onClick={decreaseCount}
-        >
-          👇
-        </span>
       </div>
     </div>
   );
