a754757f158a5db908c2bc24da89fbb9a7754f36
b_bado <bado.bernard94@gmail.com>
2020-10-10 19:07:42 +0200

    attaching listeners

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect, useRef } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const buttonRef = useRef(null);
 
-  const increaseCount = () => setCount(count + 1);
   const decreaseCount = () => count && setCount(count - 1);
+  const increaseCount = ({ target }) =>
+    target !== buttonRef.current && setCount((count) => count + 1);
+
+  // Listen for any click or key
+  useEffect(() => {
+    window.addEventListener("click", increaseCount);
+    window.addEventListener("keydown", increaseCount);
+
+    return () => {
+      window.removeEventListener("click", increaseCount);
+      window.removeEventListener("keydown", increaseCount);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Fragment>
       <div id="counter">
         <h2>{count}</h2>
-        <span role="img" aria-label="Decrease count" onClick={decreaseCount}>
+        <span
+          ref={buttonRef}
+          role="img"
+          aria-label="Decrease count"
+          onClick={decreaseCount}
+        >
           👇
         </span>
       </div>
